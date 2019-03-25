ece6f7ef4530a6818056c541ef3d2f55057e433b
haiswork <whaibin01@126.com>
2019-03-25 18:36:29 +0800

    replace package name

--- benchmark-disruptor/writer_await_test.go
+++ benchmark-disruptor/writer_await_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/smartystreets/go-disruptor"
+	"github.com/hqpko/go-disruptor"
 )
 
 func BenchmarkWriterAwaitOne(b *testing.B) {
--- example/main.go
+++ example/main.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 	"time"
 
-	"github.com/smartystreets/go-disruptor"
+	"github.com/hqpko/go-disruptor"
 )
 
 const (
